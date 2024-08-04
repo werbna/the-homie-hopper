1d2be82a9bbe917787ec87d1bf88ac6f14a9e941
Andrew <andrewchau1993@gmail.com>
2024-08-04 10:07:40 -0700

    finished user schema and added regex npm

--- models/user.js
+++ models/user.js
@@ -9,7 +9,31 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true, 
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'], // Regex for email validation
+  },
+  phoneNumber: {
+    type: String,
+  },
+  dob: {
+    type: Date,
+  },
+  address: {
+    type: String,
+  },
+  adopted: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Puppy',
+  }],
+  favorited: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Listing', 
+  }],
 });
+
 
 const User = mongoose.model('User', userSchema);
 
